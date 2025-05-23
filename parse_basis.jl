7479c8f8cf89ddcd6098ca17e1183de48e2b9912
Apolinario Miguel Tan <atan@sissa.it>
2025-05-23 19:53:45 +0200

    cleanup

--- parse_basis.jl
+++
@@ -1,7 +0,0 @@
-import Pkg
-Pkg.activate(".")
-using ACEpotentials, DelimitedFiles
-
-## Collect CLI arguments ##
-basis_tag = ARGS[1]
-println("Basis tag: $basis_tag")
--- trainmodel.jl
+++ trainmodel.jl
@@ -3,16 +3,16 @@ Pkg.activate(".")
 using ACEpotentials, DelimitedFiles
 
 ## Explicit parsing ## 
-traindir = ARGS[1] # training data load
+traindir = ARGS[1] 
 println("Training dataset taken from $traindir.")
 train_data = read_extxyz(traindir);
-if ARGS[2] == "dim" # rcut flag
+if ARGS[2] == "dim" 
     rcut = 7.0 
 else
     rcut = 5.0 # dia, gen cutoff
 end
 println("rcut = $rcut")
-if ARGS[3] == "purify" # purification flag
+if ARGS[3] == "purify" # canonical CE
     pureflag = true 
     prefix = "acejulia/" * traindir[10:end-4] * "_purify/"
 else
@@ -20,29 +20,17 @@ else
     prefix = "acejulia/" * traindir[10:end-4] * "/" 
 end
 println("Are we using the purification procedure: $pureflag.")
-elossweight= parse(Float64, ARGS[4]) # energy weight
+elossweight= parse(Float64, ARGS[4]) 
 println("(with Fcost = 1.0), Ecost = $elossweight.")
-# dampval = parse(Float64, ARGS[5]) # solver damping value
-# println("Damping value for LSQR solver = $dampval.")
 priorval = parse(Int64, ARGS[5]) # degree of smoothness prior 
 println("Degree of smoothness prior = $priorval.")
-solverflag = ARGS[6]
-println("Solver flag: $solverflag.")
 
 
 ## Control parameters ##
-# orders = [2,3,4]
-# degrees = [[16,12],[16,12,8],[16,12,8,4]]
-# basis_tags = ["16.12","16.12.8","16.12.8.4"]
 orders = [2,3,4]
-degrees = [[24,20],[24,20,16],[24,20,16,12]]
-basis_tags = ["24.20","24.20.16","24.20.16.12"]
-
-# orders = [2,3,3,3,4]
-# degrees = [[46,16],[46,16,12],[46,20,14],[46,24,16],[46,20,14,10]]
-# basis_tags  = ["46.16","46.16.12","46.20.14","46.24.16","46.20.14.10"]
+degrees = [[26,22],[26,22,18],[26,22,18,14]]
+basis_tags = ["26.22","26.22.18","26.22.18.14"]
 r0 = 1.286958464 # equilibrium length from dimer dataset
-
 
 ## Common solver properties ## 
 println("\nAssigning offset.")
@@ -57,7 +45,6 @@ train_atoms = [ACEpotentials.AtomsData(t; weights=weights, v_ref=Vref, datakeys.
 
 
 for (i, label) in enumerate(basis_tags)
-    # Basis creation
     println("\nCreating basis for order $(orders[i]), with per-correlation degrees $(degrees[i]).")
     println("r0 = $r0, rcut = $rcut.")
     basis = ACE1x.ace_basis(
@@ -70,29 +57,32 @@ for (i, label) in enumerate(basis_tags)
     println("Basis for $label created, with $(length(basis)) basis functions.")
 
     # Linear problem assembly
-    potdir = prefix * label * "/" * "ecost$(elossweight)/" * "damp$(dampval)/"
+    potdir = prefix * label * "/" * "ecost$(elossweight)/" * "prior$(priorval)/"
     println("Creating directory: $potdir")
     mkpath(potdir)
 
     println("\nAssembling linear problem elements: A, Y, W for basis set: $label.")
     A, Y, W = ACEfit.assemble(train_atoms, basis)
-    println("Creating prior.")
-    P = smoothness_prior(basis; p=2)
-    println("Creating solver.")
-    if length(ARGS) >= 6 && solverflag == "BLR"
-        solver = ACEfit.BLR()
-    else
-        solver = ACEfit.LSQR(damp = dampval, atol = 1e-6, P = P)
-    end
-    println("Solving linear problem.")
-    results = ACEfit.solve(solver, W .* A, W .* Y)
-    println("Creating potential.")
-    pot = JuLIP.MLIPs.SumIP(Vref, JuLIP.MLIPs.combine(basis, results["C"]))
-    if length(ARGS) >= 6 && solverflag == "BLR"
-        println("Saving potential at $potdir.\n\n")
-        save_potential(potdir * "potential_BLR.json", pot)
-    else
-        println("Saving potential at $potdir.\n\n")
-        save_potential(potdir * "potential.json", pot)
-    end
+    println("Creating priors and modifying A.")
+    P = smoothness_prior(basis; p=priorval)
+    Ap = Diagonal(W) * (A/P)
+    
+
+
+    # if length(ARGS) >= 6 && solverflag == "BLR"
+    #     solver = ACEfit.BLR()
+    # else
+    #     solver = ACEfit.LSQR(damp = dampval, atol = 1e-6, P = P)
+    # end
+    # println("Solving linear problem.")
+    # results = ACEfit.solve(solver, W .* A, W .* Y)
+    # println("Creating potential.")
+    # pot = JuLIP.MLIPs.SumIP(Vref, JuLIP.MLIPs.combine(basis, results["C"]))
+    # if length(ARGS) >= 6 && solverflag == "BLR"
+    #     println("Saving potential at $potdir.\n\n")
+    #     save_potential(potdir * "potential_BLR.json", pot)
+    # else
+    #     println("Saving potential at $potdir.\n\n")
+    #     save_potential(potdir * "potential.json", pot)
+    # end
 end
