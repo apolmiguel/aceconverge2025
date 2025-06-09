3ebf0bdf8857f4fe298d9c0ef3ddc14ffbc6b597
Apolinario Miguel Tan <atan@sissa.it>
2025-06-09 14:12:18 +0200

    split silicon to trian and validation sets

--- xyz_analyzer.py
+++ xyz_analyzer.py
@@ -8,9 +8,25 @@ from ase.io import read, write
 # from y2ace_funcs import *
 
 ## Data load ## 
-dset = 'Si345'
+dset = 'Si5'
 dataset = read(f'datasets/{dset}.xyz', index=':')  # ':' gets all frames
 e1b = -7881.32677981122 # isolated energy of Si atom (chong2025)
+
+
+## shuffle and split dataset to train and validation ##
+# np.random.seed(42)  # for reproducibility
+# np.random.shuffle(dataset)  # shuffle the dataset
+# split_index = int(0.9 * len(dataset))  # 90% for training
+# train_dataset = dataset[:split_index]
+# val_dataset = dataset[split_index:]  # remaining 10% for validation
+# write(f'datasets/{dset}_train.xyz', train_dataset, format='xyz')
+# write(f'datasets/{dset}_val.xyz', val_dataset, format='xyz')
+# print(f'Train dataset size: {len(train_dataset)}')
+# print(f'Validation dataset size: {len(val_dataset)}')
+# print(f'Train dataset saved to datasets/{dset}_train.xyz')
+# print(f'Validation dataset saved to datasets/{dset}_val.xyz')
+
+
 
 ## Extract distance ranges ##
 # dsets = ['Si2', 'Si3', 'Si4', 'Si5'] 
@@ -31,21 +47,21 @@ e1b = -7881.32677981122 # isolated energy of Si atom (chong2025)
 
 
 ## Extract distance histograms, generic ##
-distlist = []
-for struc in dataset[:]:
-    positions = struc.get_positions() 
-    posi = positions[:, np.newaxis, :]
-    posj = positions[np.newaxis, :, :]
-    rij_vec = posi - posj
-    rij = np.linalg.norm(rij_vec, axis=-1)
-    distlist.append(rij[np.where(rij > 1e-8)])
-print('distlist:',len(distlist))
-distlist = np.concatenate(distlist) # in case it's inhomogeneous
-distlist = np.array(distlist) # actual count is halved 
-print(distlist.shape)
-counts, bins = np.histogram(distlist.flatten(), bins=50)
-np.savetxt(f'datasets/pair_distributions/{dset}_histogram.dat',np.column_stack((bins[:-1], counts*0.5)), fmt='%.8f')
-print(f'Histogram (not double-counted) saved to datasets/pair_distributions/{dset}_histogram.dat')
+# distlist = []
+# for struc in dataset[:]:
+#     positions = struc.get_positions() 
+#     posi = positions[:, np.newaxis, :]
+#     posj = positions[np.newaxis, :, :]
+#     rij_vec = posi - posj
+#     rij = np.linalg.norm(rij_vec, axis=-1)
+#     distlist.append(rij[np.where(rij > 1e-8)])
+# print('distlist:',len(distlist))
+# distlist = np.concatenate(distlist) # in case it's inhomogeneous
+# distlist = np.array(distlist) # actual count is halved 
+# print(distlist.shape)
+# counts, bins = np.histogram(distlist.flatten(), bins=50)
+# np.savetxt(f'datasets/pair_distributions/{dset}_histogram.dat',np.column_stack((bins[:-1], counts*0.5)), fmt='%.8f')
+# print(f'Histogram (not double-counted) saved to datasets/pair_distributions/{dset}_histogram.dat')
 
 ## Si2 dimer reconstruction ## 
 # elist = []
